fcc6412d6a5d9612fc46a6121d88b4fdf6cac964
ayush-kumar-prog <ayushkumargithub@gmail.com>
2025-08-30 00:10:14 +0100

    phase 3.1 done, icon is responsive, added console logs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,65 +1,75 @@
 use tauri::{
     menu::{Menu, MenuItem},
-    tray::{MouseButton, TrayIconBuilder, TrayIconEvent},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
     Manager,
 };
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().build())
         .setup(|app| {
-            // Enable debug logging
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
-                        .build(),
-                )?;
-            }
-
-            // Create tray icon
-            let tray_menu = Menu::new(app.handle())?;
-            let settings_item = MenuItem::with_id(app, "settings", "Settings", true, None::<&str>)?;
-            let quit_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            println!("Setting up JustCall app...");
             
-            tray_menu.append(&settings_item)?;
-            tray_menu.append(&quit_item)?;
-
-            let tray = TrayIconBuilder::new()
-                .menu(&tray_menu)
-                .tooltip("JustCall")
-                .on_menu_event(move |app, event| match event.id.as_ref() {
-                    "settings" => {
-                        // Show settings window
-                        if let Some(window) = app.get_webview_window("main") {
-                            window.show().unwrap();
-                            window.set_focus().unwrap();
-                        }
-                    }
+            // Create menu items
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let settings_i = MenuItem::with_id(app, "settings", "Settings", true, None::<&str>)?;
+            
+            // Create menu
+            let menu = Menu::with_items(app, &[&settings_i, &quit_i])?;
+            
+            // Create tray icon with menu
+            let _tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .tooltip("JustCall - Right-click for menu")
+                .menu(&menu)
+                .menu_on_left_click(false) // Right-click only for menu
+                .on_menu_event(|app, event| match event.id.as_ref() {
                     "quit" => {
+                        println!("quit menu item was clicked");
                         app.exit(0);
                     }
-                    _ => {}
-                })
-                .on_tray_icon_event(|_app, event| {
-                    match event {
-                        TrayIconEvent::Click {
-                            button: MouseButton::Left,
-                            ..
-                        } => {
-                            // Left click - could show menu or toggle window
-                            println!("Tray icon clicked");
+                    "settings" => {
+                        println!("settings menu item was clicked");
+                        // Show settings window
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.show();
+                            let _ = window.set_focus();
                         }
-                        _ => {}
+                    }
+                    _ => {
+                        println!("menu item {:?} not handled", event.id);
+                    }
+                })
+                .on_tray_icon_event(|tray, event| match event {
+                    TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } => {
+                        println!("left click pressed and released");
+                        // For now, just log it
+                    }
+                    TrayIconEvent::Click {
+                        button: MouseButton::Right,
+                        ..
+                    } => {
+                        println!("right click detected");
+                    }
+                    _ => {
+                        println!("unhandled tray event: {:?}", event);
                     }
                 })
                 .build(app)?;
             
-            // Hide the main window on startup
+            println!("Tray icon created with menu!");
+            
+            // Hide main window on startup
             if let Some(window) = app.get_webview_window("main") {
-                window.hide().unwrap();
+                window.hide()?;
+                println!("Main window hidden");
             }
-
+            
             Ok(())
         })
         .run(tauri::generate_context!())
